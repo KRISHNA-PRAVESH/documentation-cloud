794fa72589462871ac79942b852a5270acd3af63
Krishna Pravesh H <krishnapravesh.h@admindroid.com>
2024-08-07 15:34:19 +0530

    changed github link for edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentation-cloud',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'KRISHNA-PRAVESH', // Usually your GitHub org/user name.
   projectName: 'documentation-cloud', // Usually your repo name.
-  
+  deploymentBranch:'gh-pages',
    
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'ignore',
@@ -71,7 +71,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/KRISHNA-PRAVESH/documentation-cloud/blob/master/',
         },
         blog: false,
         theme: {
